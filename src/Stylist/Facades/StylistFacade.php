187bdb281b994f39712da699123b338745142498
Kenny <kennytseu@barcove.my>
2021-01-29 00:40:33 +0800

    Missing Files

---
+++ src/Stylist/Facades/StylistFacade.php
@@ -0,0 +1,12 @@
+<?php
+namespace Kennytseu\Stylist\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class StylistFacade extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'stylist';
+    }
+}
---
+++ src/Stylist/Theme/Exceptions/ThemeNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kennytseu\Stylist\Theme\Exceptions;
+
+class ThemeNotFoundException extends \Exception
+{
+    public function __construct($themeName)
+    {
+        $this->message = "Theme [$themeName] is not registered with Stylist.";
+    }
+}
